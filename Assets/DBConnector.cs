cde3b8dc99583c2d3fc531a53b2de373b27a5895
foxcolony <contact@foxcolony.de>
2019-03-16 21:03:58 +0100

    Initial commit

---
+++ Assets/DBConnector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data.SQLite;
+
+public class DBConnector : MonoBehaviour
+{
+    void Start()
+    {
+        SQLiteConnection connection = new SQLiteConnection(@"Data Source=f:\mydb.db;Version=3;");
+        connection.Open();
+
+        SQLiteCommand command = connection.CreateCommand();
+        command.CommandType = System.Data.CommandType.Text;
+        command.CommandText = "CREATE TABLE IF NOT EXISTS 'highscores' ( " +
+                          "  'id' INTEGER PRIMARY KEY, " +
+                          "  'name' TEXT NOT NULL, " +
+                          "  'score' INTEGER NOT NULL" +
+                          ");";
+
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
